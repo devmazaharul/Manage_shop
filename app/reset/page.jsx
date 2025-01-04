ffd9738afeb1d1bdbcfe62a496ee556eb477122a
mazaharul islam <expertmazaharul@gmail.com>
2025-01-04 15:56:53 +0600

    change head title and desc

--- app/reset/page.jsx
+++ app/reset/page.jsx
@@ -1,9 +1,5 @@
-import { Metadata } from 'next'
+
 import React from 'react'
-export const Metadata = {
-    title: 'Reset Password',
-    description: 'Reset your password to access the dashboard and manage the system data and settings ',
-  }
 
 export default function page() {
   
